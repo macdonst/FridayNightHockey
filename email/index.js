6aae08116712081cc906aa280d107c7bdf029a59
Simon MacDonald <simon.macdonald@gmail.com>
2019-04-30 23:20:30 -0400

    ✉️  send email function

---
+++ email/index.js
@@ -0,0 +1,57 @@
+const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
+const OAuth2 = google.auth.OAuth2;
+
+module.exports = async function(context, req) {
+  const clientId = process.env.CLIENT_ID;
+  const clientSecret = process.env.CLIENT_SECRET;
+  const refresh_token = process.env.REFRESH_TOKEN;
+  const mailUser = process.env.MAIL_USER;
+
+  context.log(
+    'JavaScript HTTP trigger function processed a request using environment vars.'
+  );
+  const oauth2Client = new OAuth2(
+    clientId,
+    clientSecret,
+    'https://developers.google.com/oauthplayground'
+  );
+
+  oauth2Client.setCredentials({ refresh_token });
+
+  const tokens = await oauth2Client.refreshAccessToken();
+  const accessToken = tokens.credentials.access_token;
+
+  const smtpTransport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: mailUser,
+      clientId,
+      clientSecret,
+      refreshToken: refresh_token,
+      accessToken: accessToken
+    }
+  });
+
+  // @TODO: add error checking for the body
+  return smtpTransport
+    .sendMail(req.body)
+    .then(response => {
+      smtpTransport.close();
+      context.res = {
+        body: {
+          response
+        }
+      };
+    })
+    .catch(error => {
+      smtpTransport.close();
+      context.res = {
+        status: 500,
+        body: {
+          error
+        }
+      };
+    });
+};
